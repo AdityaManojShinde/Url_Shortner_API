bfd0bff1693aa9d303fa246a36ef32e0c4adefe6
Aditya Shinde <aditya.manoj.shinde@gmail.com>
2026-06-02 11:53:36 +0530

    fixed: tailwind config error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,8 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
 
 function App() {
   return (
     <div>
-      <Card className="max-w-sm p-2 shadow">
-        <CardTitle>Project Overview</CardTitle>
-        <CardDescription>
-          Track progress and recent activity for your Vite app.
-        </CardDescription>  
-    </Card>
+     <div className="text-2xl">Text</div>
     </div>
   )
 }
